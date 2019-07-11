fc7eaab79c8725500e392ab95a4bb43bf0f89f78
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2019-07-11 16:07:51 -0400

    Mostly working
    Currently glitches on transition 3/4, works perfectly 50% of the time and glitches the other 50%

--- Bear.java
+++ Bear.java
@@ -11,7 +11,8 @@ public class Bear extends Actor
 {
     GreenfootImage Message1 = new GreenfootImage ("Hi" , 100, Color.BLUE, Color.RED);
     GreenfootImage Message2 = new GreenfootImage ("This is cool, right?" , 50, Color.BLACK, Color.WHITE);
-    GreenfootImage Message3 = new GreenfootImage ("This is the last message" , 45, Color.GREEN, Color.YELLOW);
+    GreenfootImage Message3 = new GreenfootImage ("I will take that as a yes" , 45, Color.ORANGE, Color.BLACK);
+    GreenfootImage Message4 = new GreenfootImage ("This is the last message" , 45, Color.GREEN, Color.YELLOW);
     boolean run = true;
 
     public Bear (){
@@ -25,70 +26,57 @@ public class Bear extends Actor
     public void act() 
     {
         // Add your action code here.
-       if(run == true){
-           callMessage();
-        }
-       
-    }
-        /*
-        try
-        {
-        Thread.sleep(2000);
-        setImage(Message2);
-        Thread.sleep(3000);
-        setImage(Message3);
-        }
-        catch(InterruptedException ex)
-        {
-        Thread.currentThread().interrupt();
+
+        if(run == true){
+            callMessage();
 
         }
-        */
-       
+
+    }
+
     public void callMessage(){
-        Thread t1 = new Thread(new Runnable() 
-        { 
-            @Override
-            public void run() 
-            { 
-                if(run){
-                try
-                { 
-                    printMessage(); 
-                } 
-                catch(InterruptedException e) 
-                { 
-                    e.printStackTrace(); 
-                } 
-            }} 
-        }); 
-        
+        Thread t1 = new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        if(run){
+                            try
+                            {
+                                Printer();
+
+                            }
+                            catch(InterruptedException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }}
+                });
+
         t1.start();
     }
-       
-    public void printMessage()throws InterruptedException 
-        { 
-            // this makes the produce thread to run first. 
-            Thread.sleep(1000); 
-  
-            // synchronized block ensures only one thread 
-            // running at a time. 
-            synchronized(this) 
-            { 
-                setImage(Message2);
-  
-                // notifies the produce thread that it 
-                // can wake up. 
-                
-  
-                // Sleep 
-                Thread.sleep(2000);
-                notify(); 
-            } 
+
+    public void Printer ()throws InterruptedException{
+
+        synchronized(this)
+        {
             
-            setImage(Message3);
-            run = false;
-        } 
+            wait(2000);
+            setImage(Message2);
+
+            wait(3000);
+            
+            synchronized(this){
+                setImage(Message3);
+                wait(3000);
+                notifyAll();
+            }
+
+            notifyAll();
+        }
+
+        setImage(Message4);
+        this.run = false;
+    }
 
 }
-
